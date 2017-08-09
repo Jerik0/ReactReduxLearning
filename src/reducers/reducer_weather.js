fae65406a32e62526837d8917edee1532eae1daf
Jeriko <jcarrico1985@live.com>
2017-08-09 14:35:17 -0500

    complete functionality for graphs representing temp (in fahrenheit), pressure and humidity. replace city name with google map of city.

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -4,7 +4,6 @@ export default function(state = [], action) {
   switch (action.type) {
   case FETCH_WEATHER:
     return [ action.payload.data, ...state ]; 
-  // gives us something like [city, city, city] NOT [city, [city, city]]
   }
   return state;
 }
